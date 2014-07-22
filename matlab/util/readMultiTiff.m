eedcd0ac6a635357a22fead5dec6c0c57aa69c64
John Bogovic <bogovicj@janelia.hhmi.org>
2014-07-22 17:01:04 -0400

    can read 3d rgb tifs

--- matlab/util/readMultiTiff.m
+++ matlab/util/readMultiTiff.m
@@ -8,7 +8,14 @@ i_info = imfinfo(fn);
 
 N = numel(i_info);
 tmp = imread(fn);
+
 I = zeros( [ size(tmp) N ]);
+
+if( length(i_info(1).BitsPerSample) == 3) % RGB
+    fprintf('RGB\n');
+    I = permute( I, [1 2 4 3]);
+end
+
 for i=1:N
-    I(:,:,i) = imread( fn, i, 'Info', i_info );
+    I(:,:,i,:) = imread( fn, i, 'Info', i_info );
 end
