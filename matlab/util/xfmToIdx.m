ad8d94938ae4ee2337163501fce85ab68275e9cd
John Bogovic <bogovicj@janelia.hhmi.org>
2014-09-04 09:54:50 -0400

    xfmToIdx add flag to optionally reshape

--- matlab/util/xfmToIdx.m
+++ matlab/util/xfmToIdx.m
@@ -1,14 +1,20 @@
-function i = xfmToIdx( T, sz )
-% i = xfmToIdx( T, sz )
+function i = xfmToIdx( T, sz, rsout )
+% i = xfmToIdx( T, sz, rsout )
 %
 % Generates index lookups for a patch of size 'sz'
 % from a transformation matrix 'T'. 
+%
+% rsout - "reshape the output" (Default = true)
 %
 % See also:
 %   cubeSymmetry
 %
 % John Bogovic
 % August 2014
+
+if( ~exist( 'rsout', 'var') || isempty( rsout ))
+    rsout = 1;
+end
 
 half = (sz - 1) / 2;
 
@@ -35,7 +41,12 @@ for n = 1:length( thisT )
         args{m} = ptXfm(m,:);
     end
 
-    itmp = reshape( sub2ind( sz, args{:}), sz );
+    if( rsout )
+        itmp = reshape( sub2ind( sz, args{:}), sz );
+    else
+        itmp = sub2ind( sz, args{:} )';
+    end
+
 
     if( docell )
         i{n} = itmp;
