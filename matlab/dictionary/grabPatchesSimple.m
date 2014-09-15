cb3f3bb0b355cf4318e8c4b43404f6a4471c3118
John Bogovic <bogovicj@janelia.hhmi.org>
2014-09-15 16:43:55 -0400

    grab patches drawn from an input mask

--- matlab/dictionary/grabPatchesSimple.m
+++ matlab/dictionary/grabPatchesSimple.m
@@ -1,16 +1,27 @@
-function [patches, coords] = grabPatchesSimple( im, patchSize, numPatches )
+function [patches, coords] = grabPatchesSimple( im, patchSize, numPatches, coords, mask )
 % Usage:
-%   patches = grabPatchesSimple( im, patchSize );
+%   [patches, coords] = grabPatchesSimple( im, patchSize, numPatches, coords, mask );
 
 pNumel = prod( patchSize );
 pRad = (patchSize - 1)./2;
 sz = size( im );
 
-samplerng = [ (pRad+1)' (sz - pRad - 1)'];
-
-x = randi( samplerng(1,:), numPatches, 1);
-y = randi( samplerng(2,:), numPatches, 1);
-z = randi( samplerng(3,:), numPatches, 1);
+if( ~exist('coords','var') || isempty(coords))
+    if( ~exist('mask','var') || isempty( mask ))
+        samplerng = [ (pRad+1)' (sz - pRad - 1)'];
+        x = randi( samplerng(1,:), numPatches, 1);
+        y = randi( samplerng(2,:), numPatches, 1);
+        z = randi( samplerng(3,:), numPatches, 1);
+    else
+        j = find( mask );
+        [x,y,z] = ind2sub( size(mask), j(randi(length(j), numPatches, 1)));
+    end
+else
+    x = coords{1};
+    y = coords{2};
+    z = coords{3};
+    numPatches = length(x);
+end
 
 patches = zeros( numPatches, pNumel, class(im));
 
