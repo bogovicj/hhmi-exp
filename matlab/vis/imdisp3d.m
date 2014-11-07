fb7e09a41c0abf05a732233a9be410d1b1538ded
John Bogovic <bogovicj@janelia.hhmi.org>
2014-11-07 16:20:29 -0500

    add variable parameters to imdisp3d

--- matlab/vis/imdisp3d.m
+++ matlab/vis/imdisp3d.m
@@ -1,6 +1,6 @@
-function h = imdisp3d( im )
+function h = imdisp3d( im, varargin )
 % Usage:
 %   h = imdisp3d( im )
 
-imdisp( permute( im, [1 2 4 3]), 'border', 0.02 );
+imdisp( permute( im, [1 2 4 3]), 'border', 0.02, varargin{:} );
 h = gcf;
